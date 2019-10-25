a5c262eeac94ec5a270d5286330ce5f096f71513
goodSupper <617972470@qq.com>
2019-10-25 17:11:13 +0800

    feat: add GL-MV1000 opnwrt-sdk

---
+++ build_dir/target-aarch64_cortex-a53_musl/linux-mvebu_cortexa53/linux-4.14.131/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP Sun Sep 15 09:52:59 2019"
+#define LINUX_COMPILE_BY "zl"
+#define LINUX_COMPILE_HOST "glinet"
+#define LINUX_COMPILER "gcc version 7.4.0 (OpenWrt GCC 7.4.0 r10260-04f03e7)"
---
+++ build_dir/target-aarch64_cortex-a53_musl/linux-mvebu_cortexa53/linux-4.14.131/scripts/kconfig/.conf.cmd
@@ -0,0 +1,1 @@
+cmd_scripts/kconfig/conf := gcc  -o scripts/kconfig/conf scripts/kconfig/conf.o scripts/kconfig/zconf.tab.o -L/home/zl/mv1000_all_package/staging_dir/host/lib 
